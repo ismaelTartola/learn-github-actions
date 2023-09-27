718f3f79df006524db22b39663ce4ded5e74023c
Ismael Triana Artola <ismael.triana@ntsprint.com>
2023-09-27 11:41:20 -0500

    swagger to production

--- learn-github-actions/Program.cs
+++ learn-github-actions/Program.cs
@@ -15,14 +15,7 @@ namespace learn_github_actions
             builder.Services.AddSwaggerGen();
 
             var app = builder.Build();
-
-            // Configure the HTTP request pipeline.
-            if (app.Environment.IsDevelopment())
-            {
-                app.UseSwagger();
-                app.UseSwaggerUI();
-            }
-
+            
             app.UseHttpsRedirection();
 
             app.UseAuthorization();
